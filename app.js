db34bee09b3ecc1521906a7315cd029c36d94462
Udhav Sharma <96074679+UdhavSharma2004@users.noreply.github.com>
2022-12-21 00:37:30 +0530

    lapCount added

--- app.js
+++ app.js
@@ -2,33 +2,41 @@
 let startbtn=document.getElementById("start");
 let resetbtn=document.getElementById("reload");
 let pausebtn=document.getElementById("pause");
+let lapbtn=document.getElementById("lap");
 
 let s=0;
 let m=0;
 let h=0;
+let ms=0;
 let flag=false;
 let flagptr=null;
+let lapcount=0;
+
 function starter(){
-    s++;
-    if(s==60){
-        s=0;
-        m++;
+    ms++;
+    if(ms==100){
+        s++;
+        ms=0;
+        if(s==60){
+            s=0;
+            m++;
+            if(m==60){
+                m=0;
+                h++;
+            }
+        }
     }
-    if(m==60){
-        m=0;
-        h++;
-    }
-    document.getElementById("display").innerText=`${(h<10)?'0'+h:h} : ${(m<10)?'0'+m:m} : ${(s<10)?'0'+s:s}`
+    document.getElementById("display").innerHTML=`<p id="display">${(h<10)?'0'+h:h} : ${(m<10)?'0'+m:m} : ${(s<10)?'0'+s:s}<span id="milidisplay"> : ${(ms<10)?'0'+ms:ms}</span></p>`;
 }
-startbtn.addEventListener('click',function(){
+startbtn.addEventListener('click',()=>{
     document.getElementById('start').style.display='none';
     document.getElementById('pause').style.display='inline-block';
     if(!flag){
-        flagptr=window.setInterval(starter,1000);
+        flagptr=setInterval(starter,10);
         flag=true;
     }
 });
-pausebtn.addEventListener('click',function(){
+pausebtn.addEventListener('click',()=>{
     document.getElementById('start').style.display='inline-block';
     document.getElementById('pause').style.display='none';
     if(flag){
@@ -38,8 +46,6 @@ pausebtn.addEventListener('click',function(){
     
 });
 resetbtn.addEventListener('click',()=>{
-        document.getElementById('reload').style.animation="anim";
-        document.getElementById('reload').style.animationDuration=".5s";
         document.getElementById('start').style.display='inline-block';
         document.getElementById('pause').style.display='none';
         clearInterval(flagptr);
@@ -47,5 +53,20 @@ resetbtn.addEventListener('click',()=>{
         h=0;
         m=0;
         s=0;
+        ms=0;
         document.getElementById("display").innerText="00 : 00 : 00";
+        document.getElementById("milidisplay").innerText=" : 00"
 })
+
+lapbtn.addEventListener('click',()=>{
+    lapcount++;
+    document.getElementById("laptab").style.display="inline-block"
+    let val=document.getElementById('display').innerText+document.getElementById('milidisplay').innerText;
+    document.getElementById("laptab").innerHTML+=`<tr><td>${lapcount}</td><td>${document.getElementById('display').innerText}</td></tr>`
+
+})
+{/* <tr>
+<td>1</td>
+<td>1</td>
+<td>00:00:01</td>
+</tr> */}
